5cab0c91361ccc688fbe26ac618382b61c94ae3a
iamkumarji <shubhamk01997@gmail.com>
2021-07-04 12:14:27 +0530

    Subscribe EMail

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -233,51 +233,51 @@ This files table contents are outlined below>>>>>
 		});
 
 		// Subscribe form
-		$(".newsletter-form").validator().on("submit", function (event) {
-			if (event.isDefaultPrevented()) {
-			// handle the invalid form...
-				formErrorSub();
-				submitMSGSub(false, "Please enter your email correctly.");
-			} else {
-				// everything looks good!
-				event.preventDefault();
-			}
-		});
-		function callbackFunction (resp) {
-			if (resp.result === "success") {
-				formSuccessSub();
-			}
-			else {
-				formErrorSub();
-			}
-		}
-		function formSuccessSub(){
-			$(".newsletter-form")[0].reset();
-			submitMSGSub(true, "Thank you for subscribing!");
-			setTimeout(function() {
-				$("#validator-newsletter").addClass('hide');
-			}, 4000)
-		}
-		function formErrorSub(){
-			$(".newsletter-form").addClass("animated shake");
-			setTimeout(function() {
-				$(".newsletter-form").removeClass("animated shake");
-			}, 1000)
-		}
-		function submitMSGSub(valid, msg){
-			if(valid){
-				var msgClasses = "validation-success";
-			} else {
-				var msgClasses = "validation-danger";
-			}
-			$("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
-		}
+		// $(".newsletter-form").validator().on("submit", function (event) {
+		// 	if (event.isDefaultPrevented()) {
+		// 	// handle the invalid form...
+		// 		formErrorSub();
+		// 		submitMSGSub(false, "Please enter your email correctly.");
+		// 	} else {
+		// 		// everything looks good!
+		// 		event.preventDefault();
+		// 	}
+		// });
+		// function callbackFunction (resp) {
+		// 	if (resp.result === "success") {
+		// 		formSuccessSub();
+		// 	}
+		// 	else {
+		// 		formErrorSub();
+		// 	}
+		// }
+		// function formSuccessSub(){
+		// 	$(".newsletter-form")[0].reset();
+		// 	submitMSGSub(true, "Thank you for subscribing!");
+		// 	setTimeout(function() {
+		// 		$("#validator-newsletter").addClass('hide');
+		// 	}, 4000)
+		// }
+		// function formErrorSub(){
+		// 	$(".newsletter-form").addClass("animated shake");
+		// 	setTimeout(function() {
+		// 		$(".newsletter-form").removeClass("animated shake");
+		// 	}, 1000)
+		// }
+		// function submitMSGSub(valid, msg){
+		// 	if(valid){
+		// 		var msgClasses = "validation-success";
+		// 	} else {
+		// 		var msgClasses = "validation-danger";
+		// 	}
+		// 	$("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
+		// }
 		
-		// AJAX MailChimp
-		$(".newsletter-form").ajaxChimp({
-			url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
-			callback: callbackFunction
-		});
+		// // AJAX MailChimp
+		// // $(".newsletter-form").ajaxChimp({
+		// // 	url: "https://envytheme.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
+		// // 	callback: callbackFunction
+		// // });
 
 		// Popup Video JS 
         $('.popup-youtube, .popup-vimeo').magnificPopup({
